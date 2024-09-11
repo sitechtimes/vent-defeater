1e2b288d69af1f126dbdce20c0e0699f07f87499
Izzy-Zoltan <124058259+Izzy-Zoltan@users.noreply.github.com>
2024-09-11 11:03:43 -0400

    enter bar base
    note: need to program it so you cant type in E and you can only type up to X digits

---
+++ src/components/enterBar.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+</script>
+
+<template>
+<div class="biggerSearchBox">
+  <div class="searchBox">
+    Enter access code to join a Menti presentation
+    <input type="number" class="searchInput" placeholder="1234 5678">
+    <button class="searchButton" disabled>Join</button>
+  </div>
+</div>
+</template>
+
+<style scoped>
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+    /* display: none; <- Crashes Chrome on hover */
+    -webkit-appearance: none;
+    margin: 0; /* <-- Apparently some margin are still there even though it's hidden */
+}
+input[type=number] {
+    -moz-appearance:textfield; /* Firefox */
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import enterBar from '../components/enterBar.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <enterBar />
   </main>
 </template>
