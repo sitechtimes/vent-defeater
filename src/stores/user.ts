fb9bfa90571f80bff1891f86bfae4eb4eb943526
lawrence <91681777+whydoesthecodenotwork@users.noreply.github.com>
2024-09-12 23:20:22 -0400

    kenneth!!!!!

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -4,8 +4,5 @@ import { ref } from 'vue';
 export const userStore = defineStore('userStore', () => {
   const theme = ref<'light' | 'dark'>('light');
 
-export const userStore = defineStore('user', {
-  state: () => ({
-    theme: 'light'
-  })
+  return { theme };
 })();
