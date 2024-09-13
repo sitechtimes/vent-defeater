ac21805114ee13e1a4220873c966fbd52cf9639d
DubNubz <iamanasianboi12457@gmail.com>
2024-09-12 22:30:36 -0400

    Add Tailwind and translate CSS to Tailwind

--- src/views/app/DashboardView.vue
+++ src/views/app/DashboardView.vue
@@ -1,15 +1,20 @@
 <template>
-    <div class="page">
-        <div class="menu">
-            <img class="logo" src="/logo/logoWithWords.svg" aria-hidden="true" @click="router.push('/app/dashboard')">
-            <div class="navButtons">
-                <RouterLink v-for="button in navButtons" :key="button.name" :to="button.route">
-                    <img :src="button.img" aria-hidden="true">
-                    {{ button.name }}
-                </RouterLink>
-            </div>
-        </div>
+  <div class="flex items-center justify-center">
+    <div class="menu flex items-start justify-start flex-col w-72 min-h-screen p-8">
+      <img class="logo w-full transition duration-300 rounded-md" src="/logo/logoWithWords.svg" aria-hidden="true" @click="router.push('/app/dashboard')" />
+      <div class="flex items-center justify-center flex-col w-full">
+        <RouterLink
+          class="transition duration-300 navButton flex items-center justify-start gap-1 no-underline text-[color:var(--text-color)] w-full rounded-md"
+          v-for="button in navButtons"
+          :key="button.name"
+          :to="button.route"
+        >
+          <img class="h-7" :src="button.img" aria-hidden="true" />
+          {{ button.name }}
+        </RouterLink>
+      </div>
     </div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -17,72 +22,30 @@ import { useRouter } from 'vue-router';
 
 const router = useRouter();
 
-const navButtons = [{
-    name: "Dashboard",
-    img: "/ui/home.svg",
-    route: "/app/dashboard"
-}, {
-    name: "My presentations",
-    img: "/ui/user.svg",
-    route: "/app/presentations"
-}]
-
+const navButtons = [
+  {
+    name: 'Dashboard',
+    img: '/ui/home.svg',
+    route: '/app/dashboard'
+  },
+  {
+    name: 'My presentations',
+    img: '/ui/user.svg',
+    route: '/app/presentations'
+  }
+];
 </script>
 
 <style lang="scss" scoped>
-
-.page {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-}
-
-.menu {
-    display: flex;
-    align-items: flex-start;
-    justify-content: flex-start;
-    flex-direction: column;
-    width: 17.5em;
-    min-height: 100vh;
-    padding: 2em;
-
-    .logo {
-        width: 100%;
-    }
-}
-
-.navButtons {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-    width: 100%;
-
-    a {
-        display: flex;
-        align-items: center;
-        justify-content: flex-start;
-        gap: 0.15em;
-        text-decoration: none;
-        color: var(--text-color);
-        width: 100%;
-
-        img {
-            height: 1.75em;
-        }
-    }
-}
-
 @media (hover: hover) and (pointer: fine) {
-    .menu {
-        .logo:hover {
-            background-color: var(--faded-bg-color);
-        }
+  .menu {
+    .logo:hover {
+      background-color: var(--faded-bg-color);
     }
+  }
 
-    .navButtons {
-
-    }
+  .navButton:hover {
+    background-color: var(--faded-bg-color);
+  }
 }
-
 </style>
