31a4998e59a9af4caf542729b9cc79c94d50b520
DubNubz <iamanasianboi12457@gmail.com>
2024-09-14 02:10:28 -0400

    Update ThemeToggle.vue

--- src/components/ThemeToggle.vue
+++ src/components/ThemeToggle.vue
@@ -1,18 +1,20 @@
 <template>
-  <button @click="toggleTheme" class="bg-transparent border-0 p-0 flex items-center justify-center">
-    <img class="h-8" v-show="userStore.theme == 'light'" src="/ui/sun.svg" aria-hidden="true" />
-    <img class="h-8" v-show="userStore.theme == 'dark'" src="/ui/moon.svg" aria-hidden="true" />
+  <button @click="toggleTheme" class="bg-[color:var(--faded-bg-color)] w-14 h-8 border-solid border-2 border-[color:var(--text-color)] rounded-full relative">
+    <img class="absolute top-0.5 left-0.5 h-6 transition duration-500" :class="{ 'translate-x-6': dark }" :src="dark ? '/ui/moon.svg' : '/ui/sun.svg'" aria-hidden="true" />
   </button>
 </template>
 
 <script setup lang="ts">
 import { useUserStore } from '@/stores/user';
+import { ref } from 'vue';
 
 const userStore = useUserStore();
+const dark = ref(userStore.theme == 'dark');
 
 function toggleTheme() {
   const wasLight = userStore.theme === 'light';
   userStore.theme = wasLight ? 'dark' : 'light';
+  dark.value = wasLight;
   document.body.classList[wasLight ? 'add' : 'remove']('dark');
   localStorage.setItem('theme', userStore.theme);
 }
