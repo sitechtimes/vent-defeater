fafd93e0217d19bd1c36cafce71ff9eb7dff93f4
Izzy-Zoltan <124058259+Izzy-Zoltan@users.noreply.github.com>
2024-09-16 10:48:54 -0400

    centered div

--- src/views/app/JoinPage.vue
+++ src/views/app/JoinPage.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex items-center justify-center" >
+    <div class="flex justify-center joe" >
       <div class="flex items-center justify-center w-72 min-h-screen p-8">
         <img class="logo w-full transition duration-500 rounded-md items-center justify-center" src="/logo/logoWithWords.svg" />
       </div>
@@ -11,6 +11,8 @@
   </script>
   
   <style lang="scss" scoped>
-  
+  .joe {
+    width:100vw;
+  }
   </style>
   
