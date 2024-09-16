71cb18153da8622a7e8d0f626b1c00fb5a3720a4
Izzy-Zoltan <124058259+Izzy-Zoltan@users.noreply.github.com>
2024-09-16 10:39:50 -0400

    how to center div

---
+++ src/views/app/JoinPage.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="flex items-center justify-center" >
+      <div class="flex items-center justify-center w-72 min-h-screen p-8">
+        <img class="logo w-full transition duration-500 rounded-md items-center justify-center" src="/logo/logoWithWords.svg" />
+      </div>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+ 
+  </script>
+  
+  <style lang="scss" scoped>
+  
+  </style>
+  
