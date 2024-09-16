f7857063307642e762859ff05976da113d284c7e
Bogdan <bogdan.selyomin@gmail.com>
2024-09-16 10:15:47 -0400

    AHHHHHH WE MIHGT BNEED TO RESET PROJECT AGAIN

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -7,7 +7,16 @@ export const useUserStore = defineStore('userStore', () => {
 
   const theme = ref<'light' | 'dark'>('light');
   const presentations = ref<Presentation[]>([]);
-  const currentPresentation = ref<Presentation>();
+  const currentPres = ref<Presentation>();
 
-  return { isAuthenticated, theme, presentations, currentPresentation };
+  async function signIn(email, password) {
+    await fetch(import.meta.env.VITE_BACKEND_URL + '/auth/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email, password }),
+    })
+      .then((res) => res.json())
+    }
+
+  return { isAuthenticated, theme, presentations, currentPres, signIn };
 });
