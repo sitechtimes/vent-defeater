4f066f1de59d5cd79f236b6d2e5846787d2f4dca
Izzy-Zoltan <124058259+Izzy-Zoltan@users.noreply.github.com>
2024-09-19 10:56:37 -0400

    9/19

--- src/components/enterBar.vue
+++ src/components/enterBar.vue
@@ -1,21 +1,30 @@
 <script setup lang="ts">
-import { defineProps } from 'vue';
 
 const checkDigit = (event: KeyboardEvent) => {
   if (event.key.length === 1 && isNaN(Number(event.key))) {
     event.preventDefault();
   }
 };
+
 </script>
 
 <template>
-<div class="size-40	p-8	focus:outline-none focus:ring-0		h-40 w-40" >
-    <input type="text" class="bg-gray-200"  @keydown="checkDigit" maxlength="8" placeholder="12345678">
+<div class="joe	bg-gray-200 outline	outline-[color:var(--text-color)]" >
+    <input type="text" class="bg-transparent text-center"  @keydown="checkDigit" maxlength="8" placeholder="12345678">
 </div>
 </template>
 
 <style scoped>
+  .joe {
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    width:10rem;
+    height:5rem;
+  }
+
   *:focus {
   outline: none;
   }
+
 </style>
