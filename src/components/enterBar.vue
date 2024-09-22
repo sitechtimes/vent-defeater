44a7295ded0f43ad2131aab17a1b3c87ea357e9d
lawrence <91681777+whydoesthecodenotwork@users.noreply.github.com>
2024-09-22 12:13:16 -0400

    more entering bar 🍺

--- src/components/enterBar.vue
+++ src/components/enterBar.vue
@@ -1,27 +1,22 @@
 <script setup lang="ts">
-
 const checkDigit = (event: KeyboardEvent) => {
-  if (event.key.length === 1 && isNaN(Number(event.key))) {
-    event.preventDefault();
-  }
+  if (event.key.length === 1 && isNaN(Number(event.key))) event.preventDefault();
 };
-
 </script>
 
 <template>
-<div class="joe flex justify-center align-center rounded-lg		bg-gray-200 outline	outline-[color:var(--text-color)] focus:outline-indigo-400" >
-    <input type="text" class="bg-transparent text-center" @hover="" @keydown="checkDigit" maxlength="8" placeholder="12345678">
-</div>
+  <div class="joe flex justify-center align-center rounded-lg bg-gray-200 outline outline-[color:var(--text-color)] focus:outline-indigo-400">
+    <input type="text" class="bg-transparent text-center" @hover="" @keydown="checkDigit" maxlength="8" placeholder="12345678" />
+  </div>
 </template>
 
 <style scoped>
-  .joe {
-    width:10rem;
-    height:5rem;
-  }
+.joe {
+  width: 10rem;
+  height: 5rem;
+}
 
-  *:focus {
+*:focus {
   outline: none;
-  }
-
+}
 </style>
