66b75b2a0c9fd2b2859f125e3b94cb28e971abd8
lawrence <91681777+whydoesthecodenotwork@users.noreply.github.com>
2024-09-23 13:12:36 -0400

    more join

--- src/views/app/JoinPage.vue
+++ src/views/app/JoinPage.vue
@@ -1,28 +1,30 @@
 <template>
-      <div class=" w-screen	 flex flex-col items-center justify-center p-8 mt-50">
-        <img class="logo transition duration-500 rounded-md items-center w-80 justify-center mt-20" src="/logo/logoWithWords.svg" />
-        <h2 class="text-3xl mb-3">enter the code to join</h2>
-        <h3 class="mb-3">It’s on the screen in front of you.</h3>
-        
-        <EnterBar class="izzy"/>
-        <button class="flex items-center justify-center rounded-full bg-[color:var(--text-color)] px-10 border-r border-[color:var(--bg-color)]">
-                <p class="font-semibold text-[color:var(--bg-color)] text-lg">join!</p>
-              </button>
+  <div class="w-screen flex flex-col items-center justify-center p-8 mt-50 gap-3">
+    <img class="logo transition duration-500 rounded-md items-center w-80 justify-center mt-20" src="/logo/logoWithWords.svg" />
+    <h2 class="text-3xl">Enter the code to join</h2>
+    <h3>It’s on the screen in front of you.</h3>
 
-      </div>
-  </template>
-  
-  <script setup lang="ts">
-   import EnterBar from '../../components/EnterBar.vue';
+    <code>{{ code }}</code>
+    <div class="flex gap-2">
+      <input type="number" v-for="(char, index) in code" :id="'join-index-' + String(index)" @input="(event) => update(event, index)" class="bg-gray-200 w-[2ch] rounded-md text-4xl text-center" />
+    </div>
 
+    <button class="flex items-center justify-center rounded-full bg-[color:var(--text-color)] px-10 border-r border-[color:var(--bg-color)]">
+      <p class="font-semibold text-[color:var(--bg-color)] text-lg">John!</p>
+    </button>
+  </div>
+</template>
 
-  </script>
-  
-  <style lang="scss" scoped>
-  .izzy{
-    width:20vw;
-    margin-top:10vh;
-    margin-bottom:5vh;
-  }
-  </style>
-  
+<script setup lang="ts">
+import { ref } from 'vue';
+const code = ref('      ');
+function update(event: InputEvent, index: number) {
+  if (!event.target) return;
+  const char = event.target.value;
+  if (isNaN(Number(char))) return;
+  code.value = code.value.substring(0, index) + char + code.value.substring(index + 1);
+  console.log(code.value);
+}
+</script>
+
+<style lang="scss" scoped></style>
