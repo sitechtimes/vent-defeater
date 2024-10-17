e765760d0c891f69b44206f7a799b1760d4fe5cc
Kenneth Ng <145921389+DubNubz@users.noreply.github.com>
2024-10-17 13:11:45 -0400

    Update ResetPassword.vue

--- components/ResetPassword.vue
+++ components/ResetPassword.vue
@@ -2,6 +2,7 @@
   <div class="fixed top-0 left-0 bg-[rgba(0,0,0,0.5)] w-screen h-screen flex items-center justify-center z-50" @click="emit('close')">
     <div class="bg-[color:var(--bg-color)] flex items-center justify-center p-6 rounded-xl flex-col" @click="$event.stopPropagation()">
       <h1>Reset password</h1>
+      <p>pay $5E324 to reset your password</p>
     </div>
   </div>
 </template>
