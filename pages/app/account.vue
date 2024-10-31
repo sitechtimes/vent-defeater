99886a6e442bcca73de7e0acd19685c3318eef93
Izzy-Zoltan <124058259+Izzy-Zoltan@users.noreply.github.com>
2024-10-31 10:58:27 -0400

    happy halloween

--- pages/app/account.vue
+++ pages/app/account.vue
@@ -1,9 +1,7 @@
 <template>
-  <img class="logo w-full transition duration-300 rounded-lg cursor-pointer p-2" src="/logo/logoWithWords.svg" aria-hidden="true" @click="$router.push('/app/dashboard')" />
-  <h1 style="font-family: 'Comic Sans MS'" class="text-8xl text-center relative bottom-40 left-80">settings</h1>
   <DashboardHeader />
   <div class="flex flex-row justify-center w-screen h-full overflow-hidden">
-    <div class="menu flex items-center justify-start flex-col w-80 min-h-screen p-8 gap-8 transition duration-500 fixed left-0 top-0">
+    <div class="menu flex items-center justify-start flex-col w-80 min-h-screen p-8 gap-8 transition duration-500">
       <div class="flex items-center justify-center flex-col w-full gap-px">
         <a
           class="h-9 transition duration-300 navButton flex items-center justify-start gap-3 no-underline text-[color:var(--text-color)] w-full rounded-md"
@@ -17,31 +15,31 @@
             <!-- name length = 32 -->
           </div>
         </a>
-        <button>Save</button>
+        <button class="bg-[color:var(--faded-bg-color)] w-8/12 p-4 rounded-xl font-bold mt-8">Save</button>
       </div>
     </div>
 
-    <div class="w-10/12 flex flex-col gap-10 pt-40">
+    <div class="flex flex-grow flex-col gap-12 pt-6">
       <fieldset>
         <legend class="font-bold">General Options</legend>
         <div>
           <label for="name">Name</label>
-          <input id="name" type="text" class="rounded-md bg-[color:var(--bg-color)]" />
+          <input id="name" max-length="32" type="text" class="rounded-md bg-[color:var(--bg-color)]" />
         </div>
       </fieldset>
       <fieldset>
         <legend class="font-bold">Personal Info</legend>
         <div>
           <label for="email">Email</label>
-          <input id="email" type="text" class="rounded-md bg-[color:var(--bg-color)]" />
+          <input id="email" max-length="40" type="email" class="rounded-md bg-[color:var(--bg-color)]" />
         </div>
         <div>
           <label for="user">User</label>
-          <input id="user" type="text" class="rounded-md bg-[color:var(--bg-color)]" />
+          <input id="user" max-length="32" type="text" class="rounded-md bg-[color:var(--bg-color)]" />
         </div>
         <div>
-          <label for="password">Password</label>
-          <input id="password" type="text" class="rounded-md bg-[color:var(--bg-color)]" />
+          <label for="password">Reset password</label>
+          <input id="password" type="button" class="rounded-md bg-[color:var(--faded-bg-color)] cursor-pointer">skibidi</input>
         </div>
       </fieldset>
     </div>
@@ -91,20 +89,25 @@ const options: Category[] = [
   }
 }
 
-legend {
-  background-color: var(--faded-bg-color-light);
-  @apply rounded-md p-2 text-2xl;
+fieldaet {
+  @apply w-8/12 no-underline text-[color:var(--text-color)] bg-[color:var(--faded-bg-color-light)] rounded-md p-4 flex flex-col gap-2;
 }
 
-fieldset {
-  @apply w-11/12 no-underline text-[color:var(--text-color)] bg-[color:var(--faded-bg-color-light)] rounded-md p-4 flex flex-col gap-2;
+legend {
+  background-color: var(--faded-bg-color-light);
+  @apply rounded-3xl p-2 text-4xl;
 }
 
 fieldset div {
   @apply flex flex-col gap-2;
 }
 
+fieldset label {
+  @apply text-2xl;
+}
+
 fieldset input {
   width: 40ch;
+  @apply text-3xl;
 }
 </style>
